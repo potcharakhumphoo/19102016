d22bfb5c06404b37ecc75a42f82830266eddf6f0
potchalakhumphoo <potchala@gmail.com>
2016-10-19 11:56:55 +0700

    steup

---
+++ assets/AppAsset.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        'css/bootstrap.min1.css',
+        // เรียกใช้แบบจาก bootstrap.min จาก C:\xampp\htdocs\yii2basic19102016\web\css
+        // แบบสี โหลดมาจาก http://paintstrap.com/
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
